181c71d06902e9b501032e8f215d66c566937f76
Arva Kagdi <60667228+arvakagdi@users.noreply.github.com>
2020-06-06 19:12:06 -0400

    Update RedBlackTree.py

--- RedBlackTree.py
+++ RedBlackTree.py
@@ -1,6 +1,6 @@
-# Red Black Tree
+# Insertion in Red Black Tree
 
-class Node(object):      #Node class
+class Node(object):
     def __init__(self, value, parent, color):
         self.value = value
         self.left = None
@@ -12,15 +12,13 @@ class Node(object):      #Node class
         print_color = 'R' if self.color == 'red' else 'B'
         return '%d%s' % (self.value, print_color)
 
-
 def grandparent(node):
     if node.parent == None:
         return None
     return node.parent.parent
 
-
 # Helper for finding the node's parent's sibling
-def pibling(node):
+def uncle(node):
     p = node.parent
     gp = grandparent(node)
     if gp == None:
@@ -39,97 +37,132 @@ class RedBlackTree(object):
         yield from self.root.__iter__()
 
     def insert(self, new_val):
-        new_node = self.insert_helper(self.root, new_val)
-        self.rebalance(new_node)
+        newnode = self.insert_helper(self.root, new_val)
+        self.rebalance(newnode)
 
-    def insert_helper(self, current, new_val):
-        if current.value < new_val:
-            if current.right:
-                return self.insert_helper(current.right, new_val)
+    def insert_helper(self, curr_node, new_val):       #Basic BST insertion
+        if new_val < curr_node.value:                  # if new node is less than current node go left and search till you reach empty child
+            if curr_node.left:
+                return self.insert_helper(curr_node.left, new_val)
             else:
-                current.right = Node(new_val, current, 'red')
-                return current.right
+                curr_node.left = Node(new_val, curr_node, 'red')
+                return curr_node.left
+
         else:
-            if current.left:
-                return self.insert_helper(current.left, new_val)
-            else:
-                current.left = Node(new_val, current, 'red')
-                return current.left
+            if new_val > curr_node.value:       # if new node is greater than current node go right and search till you reach empty child
+                if curr_node.right:
+                    return self.insert_helper(curr_node.right, new_val)
+                else:
+                    curr_node.right = Node(new_val, curr_node, 'red')
+                    return curr_node.right
 
-    def rebalance(self, node):
-        # Case 1
-        if node.parent == None:
+
+    def rebalance(self,node):
+        # Case 1: if inserted node is a root node
+        if node.parent is None:
             return
 
-        # Case 2
+        # Case 2:  If parent of an inserted node is black no change required
         if node.parent.color == 'black':
             return
 
-        # Case 3
-        if pibling(node) and pibling(node).color == 'red':
-            pibling(node).color = 'black'
+        # if it did not pass previous if we know parent is red
+        # Case 3: if parent and uncle both are red
+
+        if uncle(node) and uncle(node).color == 'red':
+            uncle(node).color = 'black'
             node.parent.color = 'black'
             grandparent(node).color = 'red'
             return self.rebalance(grandparent(node))
 
         gp = grandparent(node)
-        # Small change, if there is no grandparent, cases 4 and 5
-        # won't apply
-        if gp == None:
+        if gp is None:
             return
 
-        # Case 4
-        if gp.left and node == gp.left.right:
+        # Case 4: The newly inserted node has a red parent, but that parent has a black sibling
+        # Also, both the parent of node and node parent are on different sides (LR and RL violations)
+        if gp.left and node == gp.left.right:      # ensuring it is an LR violation
             self.rotate_left(node.parent)
             node = node.left
-        elif gp.right and node == gp.right.left:
+        elif gp.right and node == gp.right.left:    # RL violation
             self.rotate_right(node.parent)
             node = node.right
 
-        # Case 5
+        # Case 5: The newly inserted node has a red parent, but that parent has a black sibling
+        # when parent and node are  in same direction  (RR and LL violations). Also change the colors of parent and gp
         p = node.parent
-        gp = p.parent
-        if node == p.left:
+        gp = grandparent(node)
+        if gp.left and node == gp.left.left:
             self.rotate_right(gp)
-        else:
+        if gp.right and node == gp.right.right:
             self.rotate_left(gp)
-        p.color = 'black'
-        gp.color = 'red'
+
+        p.color = "black"
+        gp.color = "red"
+
+
+    # Rotations
 
     def rotate_left(self, node):
-        # Save off the parent of the sub-tree we're rotating
-        p = node.parent
+        # storing the parent node
+        parentnode = node.parent
 
-        node_moving_up = node.right
-        # After 'node' moves up, the right child will now be a left child
-        node.right = node_moving_up.left
+        # node that will replace previous node
+        newnode = node.right
 
-        # 'node' moves down, to being a left child
-        node_moving_up.left = node
-        node.parent = node_moving_up
+        # if any left child of newnode exists move it to right of node
+        node.right = newnode.left
 
-        # Now we need to connect to the sub-tree's parent
-        # 'node' may have been the root
-        if p != None:
-            if node == p.left:
-                p.left = node_moving_up
+        # move node to left of new node
+        newnode.left = node
+        node.parent = newnode
+
+        # finally place the newnode as child of original parent
+        if parentnode != None:
+            if node == parentnode.left:
+                parentnode.left = newnode
             else:
-                p.right = node_moving_up
-        node_moving_up.parent = p
+                parentnode.right = newnode
+        newnode.parent = parentnode
+
 
     def rotate_right(self, node):
-        p = node.parent
+        # storing the parent node
+        parentnode = node.parent
 
-        node_moving_up = node.left
-        node.left = node_moving_up.right
+        # node that will replace previous node
+        newnode = node.left
 
-        node_moving_up.right = node
-        node.parent = node_moving_up
+        # if any right child of newnode exists move it to left   of node
+        node.left = newnode.right
 
-        # Now we need to connect to the sub-tree's parent
-        if p != None:
-            if node == p.left:
-                p.left = node_moving_up
+        # move node to right of new node
+        newnode.right = node
+        node.parent = newnode
+
+        if parentnode != None:
+            if node == parentnode.left:
+                parentnode.left = newnode
             else:
-                p.right = node_moving_up
-        node_moving_up.parent = p
+                parentnode.right = newnode
+        newnode.parent = parentnode
+
+# print function to print different test cases
+def print_tree(node, level=0):
+    print('   ' * (level - 1) + '+--' * (level > 0) + '%s' % node)
+    if node.left:
+        print_tree(node.left, level + 1)
+    if node.right:
+        print_tree(node.right, level + 1)
+
+tree = RedBlackTree(9)
+tree.insert(6)
+tree.insert(19)
+
+print_tree(tree.root)
+
+tree.insert(13)
+print_tree(tree.root)
+
+tree.insert(16)
+print_tree(tree.root)
